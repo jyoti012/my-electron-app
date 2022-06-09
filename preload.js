a966c00e212c060d8c492f1e930d6f7accd9f401
Jyoti Suvarna <40894238+jyoti012@users.noreply.github.com>
2022-06-09 19:58:58 +0530

    Add airtable pkg to make api calls

--- preload.js
+++ preload.js
@@ -1,3 +1,5 @@
+var Airtable = require('airtable');
+
 window.addEventListener('DOMContentLoaded', () => {
   const replaceText = (selector, text) => {
     const element = document.getElementById(selector)
@@ -7,4 +9,27 @@ window.addEventListener('DOMContentLoaded', () => {
   for (const dependency of ['chrome', 'node', 'electron']) {
     replaceText(`${dependency}-version`, process.versions[dependency])
   }
+
+
+  const exeCodingAppApi = async() => {
+    let base = new Airtable({apiKey: ''}).base('');
+
+    let api_result = [];
+    const records = await base('Coding app').select().all();
+    console.log("records", records);
+    records.forEach(function(record) {
+      api_result.push(record.get("Name"));
+    });
+    console.log(api_result)
+    return api_result;
+  }
+  exeCodingAppApi();
+
+  let coding_app_arr = exeCodingAppApi();
+  console.log("coding_app_arr", coding_app_arr);
+
+  // coding_app_arr.then(resp => {
+  //     coder_arr = resp
+  // })
+
 })
